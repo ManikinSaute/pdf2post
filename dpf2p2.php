bf204ee96ec8bffb413a5706946496cc1c94fa96
Thomas Parson <9444263+ManikinSaute@users.noreply.github.com>
2025-07-23 12:47:11 +0100

    Create dpf2p2.php

---
+++ dpf2p2.php
@@ -0,0 +1,7 @@
+<?php
+
+function render_pdf2p2_home_page() {
+    echo '<div class="wrap"><h1>pdf2p2 - Welcome Home!</h1>';
+	echo '<div class="wrap">Add UI here to show some info about the project</p>';
+	echo '<div class="wrap">Maybe a dashboard with some graphs :-) </p>';
+}
