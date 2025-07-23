e5b54a27dd61fc74fc05991abd1ebda3bc968d7d
Thomas Parson <9444263+ManikinSaute@users.noreply.github.com>
2025-07-23 12:31:54 +0100

    Update bulk.php

--- bulk.php
+++ bulk.php
@@ -1,1 +1,7 @@
+<?php
 
+function pdf2p2_render_bulk_import_page() {
+    echo '<div class="wrap"><h1>pdf2p2 - Bulk Imports</h1>';
+	echo '<div class="wrap">Add UI here to paste in a List of files to be processed</p>';
+	echo '<div class="wrap">Add a UI to show progress</p>';
+}
